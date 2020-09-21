5e74854cc2a6f781b2ed3d18b7e7ba887103c857
GSahar <andrey_144_sahar@mail.ru>
2020-09-21 20:00:30 +0500

    next commit menu

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -7,9 +7,11 @@ for (let menuLink of menuLinks) {
         //назначаем событие при нажатии на пункт меню
         menuLink.onclick = function () {
             //Закрываем саб меню если где-то открыто
-            closeMenu();
-            //отображаем саб меню
-            menuLink.nextElementSibling.classList.add("menu_active");
+            if (closeMenu(menuLink.nextElementSibling)) {
+                //отображаем саб меню
+                menuLink.nextElementSibling.classList.add("menu_active");
+            }
+
             //Не даем перейти по ссылке
             return false;
         }
@@ -17,13 +19,20 @@ for (let menuLink of menuLinks) {
 
 }
 
-function closeMenu() {
+function closeMenu(subMenu) {
+    let nextSubMenu = true;
     //Получаем все саб меню
     let menuSubs = document.querySelectorAll('.menu_sub');
     for (let menuSub of menuSubs) {
+
         //Если есть класс , удаляем
         if (menuSub.classList.contains("menu_active")) {
             menuSub.classList.remove("menu_active");
+            // Eсли нажали на уже открытое меню, то не добавляем снова activ
+            if (menuSub === subMenu) {
+                nextSubMenu = false;
+            }
         }
     }
+    return nextSubMenu;
 }
